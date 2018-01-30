2cc3194dab6f17865238fa3e25ccebffa5b99331
AsianDave94 <Davidtjmalmberg@gmail.com>
2018-01-30 13:28:25 +0100

    Comments

--- kaffe/FlatWhite.cs
+++ kaffe/FlatWhite.cs
@@ -7,7 +7,10 @@ using System.Threading.Tasks;
 namespace kaffe
 {
     class FlatWhite : Kaffe, Imælk
-    {
+    { /// <summary>
+    ///  interfacet inplementeres sammen med pris hvorefter man, override virtual metoden Styrke og ændre den til mild.
+    /// </summar >
+    /// <returns></returns>
         public int MlMælk()
         {
             return  160;
